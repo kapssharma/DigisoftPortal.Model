ce2f56605efe192a621dcd01246b3630c3c5a193
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-03-29 14:43:17 +0530

    Validation on wifi Model

--- Digisoft.Portal.Model/Models/Response/WiFi.cs
+++ Digisoft.Portal.Model/Models/Response/WiFi.cs
@@ -1,16 +1,27 @@
-﻿namespace DigisoftPortal.Model.Models.Response;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigisoftPortal.Model.Models.Response;
 
 public class WiFi
 {
     public int Id { get; set; }
+
+    [DataType("varchar(150)")]
+    [Required(ErrorMessage = "WiFi Name is Required")]
     public string? Name { get; set; }
 
+    [DataType("varchar(150)")]
+    [Required(ErrorMessage = "WiFi Name is Required")]
     public string? Password { get; set; }
 
+    [DataType("varchar(200)")]
+    [Required(ErrorMessage = "WiFi Provider Company Name is Required")]
     public string? ProviderCompany { get; set; }
 
     public bool StaticIP { get; set; }
 
+    [DataType("varchar(200)")]
+    [Required(ErrorMessage = "WiFi IP Address is Required")]
     public string? StaticIPAddress { get; set; }
     public DateTime CreatedOn { get; set; }
 }
