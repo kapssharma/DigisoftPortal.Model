4ecdec9c8c3cfdb061b26603660bfbb1be30712c
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-03-30 15:43:52 +0530

    add validation

--- Digisoft.Portal.Model/Models/Response/Portals.cs
+++ Digisoft.Portal.Model/Models/Response/Portals.cs
@@ -1,17 +1,35 @@
-﻿namespace DigisoftPortal.Model.Models.Response;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DigisoftPortal.Model.Models.Response;
 
 public class Portals
 {
     public int Id { get; set; }
 
+    [DataType("varchar(500)")]
+    [Required(ErrorMessage = "Field can't be empty")]
     public string? Title { get; set; }
+
+    [DataType("varchar(50)")]
+    [Required(ErrorMessage = "Field can't be empty")]
     public string? ContactPerson { get; set; }
 
+    [DataType("varchar(250)")]
+    [Required(ErrorMessage = "Field can't be empty")]
+    [EmailAddress(ErrorMessage = "Invalid Email Address")]
     public string? Email { get; set; }
 
+    [DataType("varchar(20)")]
+    [Required(ErrorMessage = "Field can't be empty")]
     public string? Phone { get; set; }
     public bool PaidService { get; set; }
     public bool Purchased { get; set; }
+
+    [Required(ErrorMessage = "Field can't be empty")]
     public DateTime ExpiredAt { get; set; }
+
+    [DataType("varchar(500)")]
+    [Required(ErrorMessage = "Field can't be empty")]
     public string? Remark { get; set; }
+    public DateTime CreatedOn { get; set; }
 }
