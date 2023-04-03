5f83bc587c06705ecd37512fbf8953a1c57a97f3
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-04-03 11:49:47 +0530

    commit for remove validation on enddate

--- Digisoft.Portal.Model/Models/Request/EventRequest.cs
+++ Digisoft.Portal.Model/Models/Request/EventRequest.cs
@@ -16,7 +16,7 @@ public class EventRequest:GenericColumns
     public bool AllDay { get; set; }
     [Required]
     public DateTime StartDate { get; set; }
-    [Required]
+   
     public DateTime EndDate { get; set; }
    
     public TimeSpan StartTime { get; set; }
