1a86426af0ad9a9b409a668b58f4c7b1b2bb8466
Vikash <developer>
2023-04-03 17:38:17 +0530

    Commit for add viewModel for UserRequest

---
+++ Digisoft.Portal.Model/Models/ViewModels/VMUserRequest.cs
@@ -0,0 +1,27 @@
+﻿using Digisoft.Portal.Web.Models;
+using DigisoftPortal.Model.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.Models.ViewModels
+{
+    public class VMUserRequest
+    {
+        public VMUserRequest()
+        {
+            UserRequest = new UserRequest();
+            SkillSet = new List<SkillSet>();
+            Designation = new List<Designation>();
+            Roles = new List<Roles>();
+        }
+        public UserRequest? UserRequest { get; set; }
+        public List<SkillSet>? SkillSet { get; set; }
+        public List<Designation>? Designation { get; set; }
+        public List<Roles>? Roles { get; set; }
+        public List<Questions>? Questions { get; set; }
+        public Modal? Modal { get; set; }
+    }
+}
