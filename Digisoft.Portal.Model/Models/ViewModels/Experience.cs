58b6bca712b7574d625c2dca2b47a999e9e94191
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-04-04 18:56:47 +0530

    commit added experiences classes

---
+++ Digisoft.Portal.Model/Models/ViewModels/Experience.cs
@@ -0,0 +1,7 @@
+﻿namespace Digisoft.Portal.Model.Models.ViewModels;
+
+public class Experience
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+}
---
+++ Digisoft.Portal.Model/Models/ViewModels/ExperienceList.cs
@@ -0,0 +1,21 @@
+﻿namespace Digisoft.Portal.Model.Models.ViewModels;
+
+public class ExperienceList
+{
+    public List<Experience> GetExperience()
+    {
+        List<Experience> experience = new List<Experience>();
+        experience.Add(new Experience { ID = 1, Name = "1" });
+        experience.Add(new Experience { ID = 2, Name = "2" });
+        experience.Add(new Experience { ID = 3, Name = "3" });
+        experience.Add(new Experience { ID = 4, Name = "4" });
+        experience.Add(new Experience { ID = 5, Name = "5" });
+        experience.Add(new Experience { ID = 6, Name = "6" });
+        experience.Add(new Experience { ID = 7, Name = "7" });
+        experience.Add(new Experience { ID = 8, Name = "8" });
+        experience.Add(new Experience { ID = 9, Name = "9" });
+        experience.Add(new Experience { ID = 10, Name = "10" });
+        return experience;
+    }
+}
+
