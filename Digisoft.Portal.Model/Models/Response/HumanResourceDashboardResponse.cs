f9e21d8a56864ca6cbc8c46dda2406d90260374e
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-04 14:47:09 +0530

    small fix

--- Digisoft.Portal.Model/Models/Response/HumanResourceDashboardResponse.cs
+++ Digisoft.Portal.Model/Models/Response/HumanResourceDashboardResponse.cs
@@ -8,13 +8,13 @@ namespace Digisoft.Portal.Model.Models.Response
 {
     public class HumanResourceDashboardResponse
     {
-        public int? Today_Leave_Counts { get; set; }
-        public int? Current_Hiring_Counts { get; set; }
-        public int? Upcoming_Events_Counts { get; set; }
-        public int? Upcoming_Leaves_Counts { get; set; }
-        public int? New_Joining_Counts { get; set; }
-        public int? Missing_Status_Counts { get; set; }
-        public int? Notified_Candidates_Counts { get; set; }
-        public int? Confirmed_Candidates_Counts { get; set; }
+        public int? Today_Leave_Count { get; set; }
+        public int? Current_Hiring_Count { get; set; }
+        public int? Upcoming_Events_Count { get; set; }
+        public int? Upcoming_Leaves_Count { get; set; }
+        public int? New_Joining_Count { get; set; }
+        public int? Missing_Status_Count { get; set; }
+        public int? Notified_Candidates_Count { get; set; }
+        public int? Confirmed_Candidates_Count { get; set; }
     }
 }
