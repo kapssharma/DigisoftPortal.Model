95ff848fd6564e425e816a93d7a7a4204085b2fa
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-04-04 19:27:03 +0530

    commit creating package classes

---
+++ Digisoft.Portal.Model/Models/ViewModels/Package.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Digisoft.Portal.Model.Models.ViewModels;
+
+public class Package
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ Digisoft.Portal.Model/Models/ViewModels/PackageList.cs
@@ -0,0 +1,20 @@
+﻿namespace Digisoft.Portal.Model.Models.ViewModels;
+
+public class PackageList
+{
+    public List<Package> GetPackage()
+    {
+        List<Package> package = new List<Package>();
+        package.Add(new Package { Id = 1, Name = "1.0" });
+        package.Add(new Package { Id = 2, Name = "2.0" });
+        package.Add(new Package { Id = 3, Name = "3.0" });
+        package.Add(new Package { Id = 4, Name = "4.0" });
+        package.Add(new Package { Id = 5, Name = "5.0" });
+        package.Add(new Package { Id = 6, Name = "6.0" });
+        package.Add(new Package { Id = 7, Name = "7.0" });
+        package.Add(new Package { Id = 8, Name = "8.0" });
+        package.Add(new Package { Id = 9, Name = "9.0" });
+        package.Add(new Package { Id = 10, Name = "10.0" });
+        return package;
+    }
+}
