15e1ec17f28b973520b33794a2d767a0d9d1d9a5
Vikash <developer>
2023-04-05 17:35:55 +0530

    Commit for update modal

--- Digisoft.Portal.Model/Models/Users/UserAddress.cs
+++ Digisoft.Portal.Model/Models/Users/UserAddress.cs
@@ -6,7 +6,6 @@ namespace DigisoftPortal.Model;
 public class UserAddress
 {
     public int ID { get; set; }
-
     public string? Address1 { get; set; }
     public string? Address2 { get; set; }
     public string? Country { get; set; }
