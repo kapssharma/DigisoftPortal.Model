3ba5a57d45ef3ac41a482092b590d91c25bf7d9f
Vikash <developer>
2023-04-05 18:09:28 +0530

    commit for change Useraddress data type

--- Digisoft.Portal.Model/ValueTypes/UserAddressType.cs
+++ Digisoft.Portal.Model/ValueTypes/UserAddressType.cs
@@ -4,7 +4,7 @@ public class UserAddressType
     public int Id { get; set; }
     public string? Address1 { get; set; }
     public string? Address2 { get; set; }
-    public int StateId { get; set; }
+    public string? StateId { get; set; }
     public string? City { get; set; }
     public string? ZipCode { get; set; }
     public bool IsParmanent { get; set; }
