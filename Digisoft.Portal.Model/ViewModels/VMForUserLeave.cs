747cf0c003f73260c13bab9f2e4264a52f13f017
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-14 17:38:13 +0530

    Commit fro model update

--- Digisoft.Portal.Model/ViewModels/VMForUserLeave.cs
+++ Digisoft.Portal.Model/ViewModels/VMForUserLeave.cs
@@ -9,20 +9,12 @@ namespace Digisoft.Portal.Model.ViewModels
 {
     public class VMForUserLeave
     {
-        public VMForUserLeave() {
+        public VMForUserLeave()
+        {
+            UserLeave = new UserLeave();
             UserLeaveDays = new List<UserLeaveDays>();
-        } 
-        public int Id { get; set; }
-        public int UserLeavesId { get; set; }
-        public string? EmployeeName { get; set; }
-        public string? LeaveReasons { get; set; }
-        public bool IsApproved { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime? ApprovedAt { get; set; }
-        public string? ApprovedBy { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-        public DateTime? CreatedOn { get; set; }
-        public bool IsActive { get; set; }
+        }
+        public UserLeave? UserLeave { get; set; }
         public List<UserLeaveDays>? UserLeaveDays { get; set; }
     }
 }
