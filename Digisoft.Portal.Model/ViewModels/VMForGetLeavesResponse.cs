6bfc92cc825585c7ef5b0f857f98399eac439b4d
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-14 15:04:53 +0530

    Commit for Add VModel for UserLives

---
+++ Digisoft.Portal.Model/ViewModels/VMForGetLeavesResponse.cs
@@ -0,0 +1,21 @@
+﻿using DigisoftPortal.Model.Models.Shared;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.ViewModels
+{
+    internal class VMForGetLeavesResponse
+    {
+        public VMForGetLeavesResponse()
+        {
+            Pagination = new Pagination();
+            VMForUserLeave = new List<VMForUserLeave>();
+        }
+        public Pagination? Pagination { get; set; }
+        public List<VMForUserLeave>? VMForUserLeave { get; set; }
+    }
+}
