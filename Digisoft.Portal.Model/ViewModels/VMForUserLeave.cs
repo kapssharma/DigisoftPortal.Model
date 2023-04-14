19a595c6e623d1e7e813cb2de4640ec952cf8b62
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-14 14:43:18 +0530

    Commit for add VMModel for user Leave List

---
+++ Digisoft.Portal.Model/ViewModels/VMForUserLeave.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.ViewModels
+{
+    public class VMForUserLeave
+    {
+        public VMForUserLeave() {
+            UserLeaveDays = new List<UserLeaveDays>();
+        } 
+        public int Id { get; set; }
+        public int UserLeavesId { get; set; }
+        public string? EmployeeName { get; set; }
+        public string? LeaveReasons { get; set; }
+        public bool IsApproved { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? ApprovedAt { get; set; }
+        public string? ApprovedBy { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? CreatedOn { get; set; }
+        public bool IsActive { get; set; }
+        public List<UserLeaveDays>? UserLeaveDays { get; set; }
+    }
+}
