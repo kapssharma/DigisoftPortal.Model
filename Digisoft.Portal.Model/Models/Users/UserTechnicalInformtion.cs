9c78eac806200bbe4c31f82b678a281e409d1486
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-19 10:58:05 +0530

    commit remove nullable

--- Digisoft.Portal.Model/Models/Users/UserTechnicalInformtion.cs
+++ Digisoft.Portal.Model/Models/Users/UserTechnicalInformtion.cs
@@ -3,7 +3,7 @@
 public class UserTechnicalInformtion
 {
     public int ID { get; set; }
-    public int? SkillsId { get; set; }
+    public int SkillsId { get; set; }
     public decimal ExpInYY { get; set; }
     public decimal ExpInMM { get; set; }
 }
