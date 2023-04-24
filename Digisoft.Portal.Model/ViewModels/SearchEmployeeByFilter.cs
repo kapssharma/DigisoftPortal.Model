a772925bebb19056a05bd8dcd06a63a2df9a67c0
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-24 13:24:27 +0530

    Commit for Update modal

---
+++ Digisoft.Portal.Model/ViewModels/SearchEmployeeByFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.ViewModels
+{
+    public class SearchEmployeeByFilter
+    {
+        public string? FullName { get; set; }
+        public DateTime? DOJ { get; set; }
+        public DateTime? LoginStart { get; set; }
+        public DateTime? LoginEnd { get; set; }
+        public DateTime? DOB { get; set; }
+        public DateTime? Status { get; set; }
+    }
+}
