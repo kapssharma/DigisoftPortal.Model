eed81211770436c5442023a02a8c2ea25e01dde2
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-04-26 14:36:42 +0530

    Commit for add UserFiltersRequest

---
+++ Digisoft.Portal.Model/Models/Request/UserFiltersRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.Models.Request
+{
+    public class UserFiltersRequest
+    {
+        public string? SearchColumn { get; set; } = "";
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? DOB { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? JoiningDate { get; set; }
+        public string? SearchValue { get; set; } = "";
+        public int PageNo { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public string? SortColumn { get; set; } = "";
+        public string? SortOrder { get; set; } = "";
+        public bool IsActive { get; set; } = true;
+    }
+}
