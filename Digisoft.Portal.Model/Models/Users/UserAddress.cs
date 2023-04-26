003e959186946e37b3654b1a23968c1ddc6d0033
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-04-26 10:02:13 +0530

    commit for address validation

--- Digisoft.Portal.Model/Models/Users/UserAddress.cs
+++ Digisoft.Portal.Model/Models/Users/UserAddress.cs
@@ -6,10 +6,15 @@ namespace DigisoftPortal.Model;
 public class UserAddress
 {
     public int ID { get; set; }
+    [Required]
     public string? Address1 { get; set; }
+    [Required]
     public string? Address2 { get; set; }
+    [Required]
     public string? Country { get; set; }
+    [Required]
     public string? StateId { get; set; }
+    [Required]
     public string? City { get; set; }
     [Required(ErrorMessage = "Zip is Required")]
     [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Zip")]
