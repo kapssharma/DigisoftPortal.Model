db7f7f0fe2f0003af5821bca37b90e7161377ffc
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-04-28 13:38:36 +0530

    commit for user validation

--- Digisoft.Portal.Model/Models/Request/UserRequest.cs
+++ Digisoft.Portal.Model/Models/Request/UserRequest.cs
@@ -14,26 +14,30 @@ public class UserRequest:GenericColumns
     public string? LastName { get; set; }
     [DataType("nvarchar(50)")]
     [Required(ErrorMessage = "Phone Number Required!")]
-    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$",ErrorMessage = "Entered phone format is not valid.")]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
     public string? PhoneNumber { get; set; }
+
     [DataType("nvarchar(50)")]
     [Required(ErrorMessage = "Emergency Number Required!")]
     [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Entered phone format is not valid.")]
     public string? AlternativePhoneNumber { get; set; }
-    [DataType("nvarchar(50)")]
 
+    [DataType("nvarchar(50)")]
     public string? VoterId { get; set; }
+
     [DataType("nvarchar(50)")]
     [Required(ErrorMessage = "Aadhaar Number is required.")]
     [RegularExpression(@"^([0-9]{4}[0-9]{4}[0-9]{4}$)|([0-9]{4}\s[0-9]{4}\s[0-9]{4}$)|([0-9]{4}-[0-9]{4}-[0-9]{4}$)", ErrorMessage = "Invalid Aadhaar Number.")]
     public string? AadharCardNo { get; set; }
+
     [DataType("nvarchar(50)")]
     [Required(ErrorMessage = "PAN Number is required")]
     [RegularExpression("^([A-Za-z]){5}([0-9]){4}([A-Za-z]){1}$", ErrorMessage = "Invalid PAN Number")]
     public string? Pancard { get; set; }
-    [DataType("nvarchar(50)")]
+
     [Required(ErrorMessage = "Email is required.")]
     [RegularExpression(@"^([A-Za-z0-9][^'!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ][a-zA-z0-9-._][^!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ]*\@[a-zA-Z0-9][^!&@\\#*$%^?<> ()+=':;~`.\[\]{}|/,₹€ ]*\.[a-zA-Z]{2,6})$", ErrorMessage = "Please enter a valid Email")]
+    [DataType(DataType.EmailAddress)]
     public string? Email { get; set; }
     public string? Password { get; set; }
 
@@ -43,7 +47,7 @@ public class UserRequest:GenericColumns
     public string? OfficialEmail { get; set; }
     public string? ProfileImage { get; set; }
 
-    [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString = "{0:MM/dd/yyyy}")]
+    [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
     public DateTime? DOB { get; set; }
 
     public int BloodGroup { get; set; }
@@ -69,5 +73,4 @@ public class UserRequest:GenericColumns
     public List<UserDocuments>? UserDocuments { get; set; }
     public List<UserCompany>? UserCompany { get; set; }
     public List<UserGetTOKnows>? UserGetTOKnows { get; set; }
-
 }
