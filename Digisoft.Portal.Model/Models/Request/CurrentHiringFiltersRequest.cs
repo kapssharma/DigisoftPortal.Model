2d43e9edb6144ad1ed5391d082603610176660a2
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-05-02 13:07:38 +0530

    commit for creating hiring filter request

---
+++ Digisoft.Portal.Model/Models/Request/CurrentHiringFiltersRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Digisoft.Portal.Model.Models.Request;
+
+public class CurrentHiringFiltersRequest
+{
+    public string? Title { get; set; }
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime? StartDate { get; set; }
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime? ClosingDate { get; set; }
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+    public DateTime? CreatedOn { get; set; }
+    public string? SkillSet { get; set; }
+    public decimal? MaxPackage { get; set; }
+    public decimal? MinPackage { get; set; }
+    public string? SearchValue { get; set; } = "";
+    public int PageNo { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? SortColumn { get; set; } = "";
+    public string? SortOrder { get; set; } = "";
+    public bool IsActive { get; set; } = true;
+}
