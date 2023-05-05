a8f96ffd7f4c510a1419e535c861ee16b60cc15b
NIPUN.TRIPATHI <nipun.tripathi@glennissolutions.com>
2023-05-05 10:05:43 +0530

    Commit for add new class FiltersLeavesRequest

---
+++ Digisoft.Portal.Model/Models/Request/FiltersLeavesRequest.cs
@@ -0,0 +1,22 @@
+﻿using DigisoftPortal.Model.Models.Request;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digisoft.Portal.Model.Models.Request
+{
+    public class FiltersLeavesRequest : FiltersRequest
+    {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? StartFrom { get; set; }
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? StartEnd { get; set; }
+        public int LeaveReasons { get; set; }
+        public int LeaveType { get; set; }
+        public int ApprovedBy { get; set; }
+    }
+}
