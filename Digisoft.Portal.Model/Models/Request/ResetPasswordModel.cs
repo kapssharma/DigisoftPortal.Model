9abcfee6a68edfbf6e2dba8fee174b46c5a2d95a
MonaMaurya <mona.maurya@digisoftsolution.net>
2023-05-22 10:31:31 +0530

    commit for added password models

---
+++ Digisoft.Portal.Model/Models/Request/ResetPasswordModel.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace Digisoft.Portal.Model.Models.Request;
+
+public class ResetPasswordModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "New password required", AllowEmptyStrings = false)]
+    [DataType(DataType.Password)]
+    public string NewPassword { get; set; }
+
+    [DataType(DataType.Password)]
+    [Compare("NewPassword", ErrorMessage = "New password and confirm password does not match")]
+    public string ConfirmPassword { get; set; }
+
+    [Required]
+    public string ResetCode { get; set; }
+
+}
